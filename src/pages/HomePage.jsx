a441c03aa8ca3759777e7eb57af450b03b90825a
Darell Rangga <darrelrangga@gmail.com>
2025-06-28 16:07:23 +0700

    Implement HomePage layout with header content and styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,34 @@
+import { Container, Row, Col } from "react-bootstrap";
+import HeroImage from "../assets/img/hero.png";
+
 export default function HomePage() {
   return (
     <div className="homepage">
-      <header className="w-100 min-vh-100"></header>
+      <header className="w-100 min-vh-100 d-flex align-items-center">
+        <Container>
+          <Row className="header-box d-flex align-items-center">
+            <Col lg="6">
+              <h1 className="mb-4">
+                Temukan <br /> <span>Bakat Anda Di Sini !</span> <br /> Bersama
+                Kami
+              </h1>
+              <p className="mb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+                soluta odio placeat iste natus pariatur!
+              </p>
+              <button className="btn btn-danger btn-lg rounded-1 me-2 mb-xs-0 mb-2">
+                Lihat Kelas
+              </button>
+              <button className="btn btn-outline-danger btn-lg rounded-1 mb-xs-0 mb-2">
+                Lihat Promo
+              </button>
+            </Col>
+            <Col lg="6" className="pt-lg-0 pt-5">
+              <img src={HeroImage} alt="HeroImg" />
+            </Col>
+          </Row>
+        </Container>
+      </header>
       <div className="kelas w-100 min-vh-100"></div>
     </div>
   );
