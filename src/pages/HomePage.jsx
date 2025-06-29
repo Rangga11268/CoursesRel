7bae17ad1678ed76169d78ac612a2c156c300596
Darell Rangga <darrelrangga@gmail.com>
2025-06-29 12:55:54 +0700

    Refactor HomePage component to include navigation and dynamic class rendering; add main CSS file for styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import { Container, Row, Col } from "react-bootstrap";
 import HeroImage from "../assets/img/hero.png";
 import { kelasTerbaru } from "../data";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+  let navigate = useNavigate();
   return (
     <div className="homepage">
       <header className="w-100 min-vh-100 d-flex align-items-center">
@@ -50,16 +52,34 @@ export default function HomePage() {
                     alt="unsplash.com"
                     className="w-100 mb-5 rounded-top"
                   />
-                  <div className="star mb-2">
+                  <div className="star mb-2 px-3">
                     <i className={kelas.star1}></i>
                     <i className={kelas.star2}></i>
                     <i className={kelas.star3}></i>
                     <i className={kelas.star4}></i>
                     <i className={kelas.star5}></i>
                   </div>
+                  <h5 className="mb-5 px-3">{kelas.title}</h5>
+                  <div className="ket d-flex justify-content-between align-items-center px-3 pb-3">
+                    <p className="m-0 text-primary fw-bold">{kelas.price}</p>
+                    <button className="btn btn-danger rounded-1">
+                      {kelas.buy}
+                    </button>
+                  </div>
                 </Col>
               );
             })}
+          </Row>
+          <Row>
+            <Col className="text-center">
+              <button
+                className="btn btn-success rounded-5 btn-lg"
+                onClick={() => navigate("/kelas")}
+              >
+                Lihat Semua kelas{" "}
+                <i className="fa-solid fa-chevron-right ms-1"></i>
+              </button>
+            </Col>
           </Row>
         </Container>
       </div>
