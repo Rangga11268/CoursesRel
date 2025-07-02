7fa3add47efbd16790e1fe2940643fe2b411ae60
Darell Rangga <darrelrangga@gmail.com>
2025-07-02 16:43:12 +0700

    Add styles for SyaratPage and enhance layout with additional content

--- src/pages/SyaratPage.jsx
+++ src/pages/SyaratPage.jsx
@@ -4,7 +4,7 @@ import FaqComponent from "../components/FaqComponent";
 export default function SyaratPage() {
   return (
     <div className="syarat-page">
-      <div className="syarat-ketentuan">
+      <div className="syarat-ketentuan min-vh-100">
         <Container>
           <Row>
             <Col>
@@ -15,8 +15,102 @@ export default function SyaratPage() {
               </p>
             </Col>
           </Row>
+          <Row className="pt-5">
+            <Col>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Tenetur ratione delectus sint? Necessitatibus aliquam
+                repellendus commodi eligendi cumque! Reprehenderit magnam
+                voluptatem harum velit dignissimos iure, cum dolor id
+                accusantium doloremque molestiae corrupti eaque eum sapiente
+                molestias labore officia recusandae delectus ducimus eveniet?
+                Nisi eum reprehenderit laboriosam necessitatibus officia ad
+                veritatis.
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">1. Lorem, ipsum.</h4>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima
+                iusto ipsa veritatis fugit facere commodi voluptatum deserunt
+                ab, eligendi natus deleniti recusandae vitae quos repudiandae
+                ducimus voluptas perferendis tempore nisi veniam! Veniam nisi
+                rerum accusantium dolorum voluptates voluptate fugit dicta.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio
+                voluptates consequuntur quidem quaerat reprehenderit esse cum id
+                iusto aut, expedita delectus distinctio culpa consectetur
+                eligendi non soluta ullam quo voluptatum? Officiis ipsum nobis
+                necessitatibus. Quidem cum illo distinctio? Impedit, tenetur!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Molestiae placeat vitae numquam quibusdam totam natus nesciunt
+                dolores, eum sit nisi aliquam atque officiis harum doloribus
+                unde sapiente ad dignissimos vel fugiat, voluptate, eligendi
+                distinctio temporibus qui! Dolorem doloribus minima earum sunt
+                quae reiciendis dolor aperiam eius maiores ab, suscipit nulla!
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">2. Lorem, ipsum.</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos
+                labore autem, rerum et repudiandae, numquam aperiam error
+                incidunt est unde id commodi nostrum mollitia. Suscipit numquam
+                tenetur nihil dignissimos quia ipsa ipsum modi provident enim
+                ducimus fugiat, sunt quidem id asperiores, iusto esse rerum
+                excepturi possimus illum quibusdam est vel.
+              </p>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Exercitationem numquam minus officiis earum doloribus in optio
+                laudantium molestiae. In alias laborum facere dignissimos veniam
+                odit ipsum assumenda impedit dolores blanditiis dolorum maxime
+                unde eligendi repellendus, temporibus quo itaque laboriosam
+                perferendis! Mollitia, laudantium architecto. Explicabo
+                repellendus illo minus sapiente vitae magni.
+              </p>
+            </Col>
+          </Row>
+          <Row className="py-3">
+            <Col>
+              <h4 className="fw-bold">3. Lorem, ipsum.</h4>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Temporibus, ullam. Nam accusantium dicta delectus enim laborum
+                excepturi eos quasi iste est blanditiis, quam, placeat fuga in
+                repellat. Nihil harum voluptatibus, minus necessitatibus,
+                possimus vero odio quos atque et dolorum repellendus? Voluptatum
+                eum officiis consequuntur dolore inventore tempore minima nemo
+                architecto!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+                itaque aperiam molestiae reprehenderit repellendus quod
+                similique eius doloribus perferendis distinctio libero ea
+                impedit veniam architecto esse magni, sit repellat. Consequatur
+                repellat ipsum omnis sequi eaque velit dolorum ullam porro,
+                eligendi aperiam libero eveniet, quisquam quaerat expedita cum
+                ratione excepturi perspiciatis!
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Cupiditate nostrum, assumenda illum dolorem ea non impedit ad
+                mollitia aperiam voluptates architecto qui ut cum aliquid fugiat
+                quas aspernatur expedita? Quae reiciendis officia at ullam
+                doloremque dolorem eaque ut ratione aut.
+              </p>
+            </Col>
+          </Row>
         </Container>
       </div>
+      <FaqComponent />
     </div>
   );
 }
