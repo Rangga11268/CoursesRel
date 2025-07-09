0aa42833740738d4bffd67c6b2fccb1f909c782a
Darell Rangga <darrelrangga@gmail.com>
2025-07-09 12:31:22 +0700

    Add animate.css for animations and implement ScrollToTop component; enhance HomePage with animations

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./dist/css/main.css";
+import "animate.css";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>
 );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,27 +14,34 @@ export default function HomePage() {
   let navigate = useNavigate();
   return (
     <div className="homepage">
-      <header className="w-100 min-vh-100 d-flex align-items-center">
+      <header className="w-100 min-vh-100 d-flex align-items-center overflow-hidden">
         <Container>
           <Row className="header-box d-flex align-items-center pt-lg-5">
             <Col lg="6">
-              <h1 className="mb-4">
+              <h1 className="mb-4 animate__animated animate__fadeInUp animate__delay-1s">
                 Temukan <br /> <span>Bakat Anda Di Sini !</span> <br /> Bersama
                 Kami
               </h1>
-              <p className="mb-4">
+              <p className="mb-4 mb-4 animate__animated animate__fadeInUp animate__delay-1s">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
                 soluta odio placeat iste natus pariatur!
               </p>
-              <button className="btn btn-danger btn-lg rounded-1 me-2 mb-xs-0 mb-2">
+              <button
+                className="btn btn-danger btn-lg rounded-1 me-2 mb-xs-0 mb-2 mb-4 animate__animated animate__fadeInUp animate__delay-1s"
+                onClick={() => navigate("/kelas")}
+              >
                 Lihat Kelas
               </button>
-              <button className="btn btn-outline-danger btn-lg rounded-1 mb-xs-0 mb-2">
+              <button className="btn btn-outline-danger btn-lg rounded-1 mb-xs-0 mb-2 mb-4 animate__animated animate__fadeInUp animate__delay-1s">
                 Lihat Promo
               </button>
             </Col>
             <Col lg="6" className="pt-lg-0 pt-5">
-              <img src={HeroImage} alt="HeroImg" />
+              <img
+                src={HeroImage}
+                alt="HeroImg"
+                className="animate__animated animate__fadeInUp"
+              />
             </Col>
           </Row>
         </Container>
